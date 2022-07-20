2c58122082c260ddc8df7950293c2469970f2ebd
wagner <you@example.com>
2022-07-20 01:49:25 -0300

    Second Commmit

---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if(isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString()); 
+            }
+
+            return BadRequest("Invalid Imput");
+        }
+
+        private bool isNumeric(string strNUmber)
+        {
+            double number;
+
+            bool isNumber = double.TryParse(
+                strNUmber, System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+
+            return isNumber;
+        }
+
+        private decimal  ConvertToDecimal(string strNUmber)
+        {
+            decimal decimalValue;
+
+            if(decimal.TryParse(strNUmber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+ 
+    }
+}
