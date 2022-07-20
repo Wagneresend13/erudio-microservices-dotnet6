0c1709f308e7b32bc08ff739227c141e5581afa0
wagner <you@example.com>
2022-07-20 18:21:11 -0300

    Terceiro commit

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -24,8 +24,8 @@ namespace RestWithASPNETUdemy.Controllers
         {
             if(isNumeric(firstNumber) && isNumeric(secondNumber))
             {
-                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString()); 
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);                                
+                return Ok(sum.ToString());       
             }
 
             return BadRequest("Invalid Imput");
@@ -42,12 +42,45 @@ namespace RestWithASPNETUdemy.Controllers
             return BadRequest("Invalid Imput");
         }
 
-        [HttpGet("divisao/{firstNumber}/{secondNumber}")]
-        public IActionResult GetDivisao(string firstNumber, string secondNumber)
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult GetMultiplication(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var divisao = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(divisao.ToString());
+            }
+            return BadRequest("Invalid Imput");
+        }
+
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Getdivision(string firstNumber, string secondNumber)
         {
             if (isNumeric(firstNumber) && isNumeric(secondNumber))
             {
                 var divisao = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(divisao.ToString());
+            }
+            return BadRequest("Invalid Imput");
+        }
+
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public IActionResult GetMean(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var divisao = (ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber)) / 2;
+                return Ok(divisao.ToString());
+            }
+            return BadRequest("Invalid Imput");
+        }
+
+        [HttpGet("SquareRoot/{firstNumber}")]
+        public IActionResult GetSquareRoot(string firstNumber)
+        {
+            if (isNumeric(firstNumber))
+            {
+                var divisao = Math.Sqrt((double)ConvertToDecimal(firstNumber));
                 return Ok(divisao.ToString());
             }
             return BadRequest("Invalid Imput");
