296168a0ca3528e5f67026330ca2f5dcbcc54435
wagner <you@example.com>
2022-10-29 10:04:45 -0300

    18 commit

--- S008_EdudioMicroservices.NET6-OrganizingFirstMicroServicesArchitect/GeekShooping/GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ S008_EdudioMicroservices.NET6-OrganizingFirstMicroServicesArchitect/GeekShooping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using GeekShopping.ProductAPI.Data.ValueObjects;
 using GeekShopping.ProductAPI.Repository;
+using GeekShopping.ProductAPI.Utils;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.ProductAPI.Controllers
@@ -11,13 +13,13 @@ namespace GeekShopping.ProductAPI.Controllers
 
         private IProductRepository _repository;
 
-
         public ProductController(IProductRepository repository)
         {
             _repository = repository ?? throw new ArgumentException(nameof(repository));
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
         {
             var products = await _repository.FindAll();
@@ -25,6 +27,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpGet("{id}")]
+        [Authorize]
         public async Task<ActionResult<ProductVO>> FindById(long id)
         {
             var product = await _repository.FindById(id);
@@ -33,6 +36,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPost]
+        [Authorize]
         public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO vo)
         {
            
@@ -42,6 +46,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
+        [Authorize]
         public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO vo)
         {
 
@@ -51,6 +56,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpDelete("{id}")]
+        [Authorize(Roles = Role.Admin)]
         public async Task<ActionResult> Delete(long id)
         {
             var status = await _repository.Delete(id);
